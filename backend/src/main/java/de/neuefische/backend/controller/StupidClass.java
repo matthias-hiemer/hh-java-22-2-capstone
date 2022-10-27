4ea65b1cd28d866ea562ac01fbeec2f5bc79bc07
Matthias Hiemer <matthias.hiemer@neuefische.de>
2022-10-27 11:23:28 +0200

    Add code with code smell to test Sonar

---
+++ backend/src/main/java/de/neuefische/backend/controller/StupidClass.java
@@ -0,0 +1,18 @@
+package de.neuefische.backend.controller;
+
+public class StupidClass {
+
+    // commented code
+   /*
+    public int add(int a, int b) {
+        return a+b;
+    }
+    */
+
+    public int Add(int a, int b) {
+                        return
+                a+b;
+    }
+
+
+}
